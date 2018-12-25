e0861934e68b67875e7dbbd6e4d4f7065f146da7
Max Desiatov <max@desiatov.com>
2018-12-25 14:53:53 +0100

    Add properties and docs to TestView

--- Sources/GluonTestRenderer/TestView.swift
+++ Sources/GluonTestRenderer/TestView.swift
@@ -7,6 +7,22 @@
 
 import Gluon
 
-public class TestView {
-
+/// A class that `TestRenderer` uses as a target.
+/// When rendering to a `TestView` instance it is possible
+/// to examine its `subviews` and `props` for testing.
+public final class TestView {
+  /// Subviews of this test view.
+  public private(set) var subviews = [TestView]()
+  
+  /// Props assigned to this test view.
+  public internal(set) var props: AnyEquatable
+  
+  /// Initialize a new test view.
+  init(props: AnyEquatable) {
+    self.props = props
+  }
+  
+  func add(subview: TestView) {
+    subviews.append(subview)
+  }
 }
