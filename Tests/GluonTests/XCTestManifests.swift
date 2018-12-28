9113db3b92a2cc48836c124144604b988454ce9a
Max Desiatov <max@desiatov.com>
2018-12-28 22:04:23 +0000

    Update .swiftformat rules

--- Tests/GluonTests/XCTestManifests.swift
+++ Tests/GluonTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
 #if os(Linux)
-  public func allTests() -> [XCTestCaseEntry] {
-    return [
-      testCase(GluonTests.allTests),
-    ]
-  }
+public func allTests() -> [XCTestCaseEntry] {
+  return [
+    testCase(GluonTests.allTests),
+  ]
+}
 #endif
