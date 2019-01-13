2ee426b027e308aff9f447b2a1cda97fd037e92f
Max Desiatov <max@desiatov.com>
2019-01-13 22:15:59 +0000

    Remove unused hooks code

--- Sources/Gluon/Hooks.swift
+++ Sources/Gluon/Hooks.swift
@@ -32,27 +32,6 @@ public struct Hooks {
   /// This overload works around unexpected behaviour, where
   /// `"\(#file)\(#line)"` as a default arguments is evaluated to local values,
   /// not values at the call site.
-//  public func state<T>(_ initial: T,
-//                       file: String = #file,
-//                       line: Int = #line) -> (T, (T) -> ()) {
-//    return state(initial, id: "\(file)\(line)")
-//  }
-//
-//  public func state<T>(_ initial: T,
-//                       id: String) -> (T, (T) -> ()) {
-//    guard let currentState = currentState,
-//      let queueState = queueState else {
-//      fatalError("""
-//        attempt to use `state` hook outside of a `render` function,
-//        or `render` is not called from a renderer
-//      """)
-//    }
-//
-//    let current = currentState(id) as? T ?? initial
-//
-//    return (current, { queueState($0, id) })
-//  }
-
   public func state<T>(_ initial: T,
                        file: String = #file,
                        line: Int = #line) -> State<T> {
