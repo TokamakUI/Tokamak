11ac1cc18b395d80011ad6f117213a3ab48c9e5c
Max Desiatov <max@desiatov.com>
2019-02-10 18:20:33 +0000

    Improve doc comments on Hooks.effect overload

--- Sources/Gluon/Hooks/Effect.swift
+++ Sources/Gluon/Hooks/Effect.swift
@@ -37,6 +37,24 @@ extension Hooks {
     scheduleEffect?(AnyEquatable(observed), { closure(); return nil })
   }
 
+  /** Schedule an effect to be executed on calls to `render` when `observed`
+   value has changed from the previous call to `render`. The effect
+   closure should return a cleanup closure to be executed before the next
+   call to `render` or when a component is unmounted.
+
+   You can use this overload of `effect` to control when exactly the effect
+   `closure` is executed with additional cleanup. For example, always pass
+   `Null()` as `observed` so that the effect is executed only once on the
+   initial rendering.
+
+   Another use case is an effect that subscribes to updates on a user model
+   fetched from the network and you need to correctly unsubscribe from updates
+   when a component is unmounted. You wouldn't to unsubscribe and resubscribe
+   every time a component rerendered if an observed user ID hasn't changed.
+   Pass the ID as `observed`, which will be compared to the previous value and
+   trigger effect execution (unsubscribing from updates on old user ID and
+   subscribing for new user ID) when the ID has changed.
+   */
   public func effect<T: Equatable>(_ observed: T, closure: () -> () -> ()) {
     scheduleEffect?(AnyEquatable(observed), closure)
   }
