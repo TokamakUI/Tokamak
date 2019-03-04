2818669601b9e2d50374366c030238d4394f2a7d
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-03-04 21:19:53 +0200

    Update Scroll extension

--- Example/Tokamak/Components/Scoll.swift
+++ Example/Tokamak/Components/Scoll.swift
@@ -12,9 +12,8 @@ struct ScrollViewExample: LeafComponent {
   typealias Props = Null
 
   static func render(props: Props, hooks: Hooks) -> AnyNode {
-    let ref = hooks.ref(type: UIScrollView.self)
     var labels: [AnyNode] = []
-    for i in 1 ..< 100 {
+    for i in 1..<100 {
       labels.append(Label.node(.init(alignment: .center), "Text \(i)"))
     }
     return View.node(
@@ -24,14 +23,17 @@ struct ScrollViewExample: LeafComponent {
             Width.equal(to: .parent),
             Height.equal(to: .parent),
             CenterX.equal(to: .parent),
-            ]
+          ]
         )
       ),
       ScrollView.node(
-        ref: ref,
         .init(
           Style(
             [
+              Edges.equal(to: .parent),
+              Top.equal(to: .parent),
+              Trailing.equal(to: .parent),
+              Leading.equal(to: .parent),
               Width.equal(to: .parent),
               Height.equal(to: .parent),
               CenterX.equal(to: .parent),
@@ -44,8 +46,7 @@ struct ScrollViewExample: LeafComponent {
             Height.equal(to: .parent),
             CenterX.equal(to: .parent),
           ],
-          axis: .vertical
-          ),
+                axis: .vertical),
           labels
         )
       )
