25fa624d7be1cd94cbd0c9815a6d57faa4d6c8ac
Max Desiatov <max@desiatov.com>
2019-03-20 13:01:41 +0000

    Add Throbber host component (#71)
    This component is rendered to `UIActivityViewIndicator` in `TokamakUIKit`. This component would be a great helper for future examples that demonstrate asynchronous effects, e.g. networking.
    
    * Add Throbber host component
    * Make .gray Throbber variety the default

---
+++ Example/Tokamak/Components/Throbber.swift
@@ -0,0 +1,29 @@
+//
+//  Throbber.swift
+//  TokamakDemo-iOS
+//
+//  Created by Max Desiatov on 20/03/2019.
+//  Copyright © 2019 Tokamak contributors. Tokamak is available under the
+//  Apache 2.0 license. See the LICENSE file for more info.
+//
+
+import Tokamak
+
+struct ThrobberExample: PureLeafComponent {
+  typealias Props = Null
+
+  static func render(props: Null) -> AnyNode {
+    return
+      View.node(
+        .init(Style(
+          Edges.equal(to: .parent),
+          backgroundColor: .black
+        )),
+        Throbber.node(.init(
+          Style(Edges.equal(to: .parent)),
+          isAnimating: true,
+          variety: .whiteLarge
+        ))
+      )
+  }
+}
