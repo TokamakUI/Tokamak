b2cd397b28d4926b982562858cee5278e5756cff
matvii <matvii@hodovani.uk>
2019-03-27 18:50:13 +0200

    Add applyScrollOptions to UIScrollView components (#75)
    * Add applyScrollOptions to UIScrollView components
    
    * Add ScrollOptions to CollectionView, ListView, ScrollView
    
    * Rename scrollProps to scrollOptions
    
    * Change ScrollOptions init in Example
    
    * Refactor ScrollView
    
    * Remove storage ScrollOptions in ScrollView

--- Sources/Tokamak/Components/Host/ScrollView.swift
+++ Sources/Tokamak/Components/Host/ScrollView.swift
@@ -11,48 +11,8 @@ public struct ScrollView: HostComponent {
       return Props()
     }
 
-    public struct EdgeInsets: Equatable {
-      public let bottom: Float
-      public let left: Float
-      public let right: Float
-      public let top: Float
-
-      public init(
-        top: Float = 0,
-        left: Float = 0,
-        bottom: Float = 0,
-        right: Float = 0
-      ) {
-        self.top = top
-        self.left = left
-        self.bottom = bottom
-        self.right = right
-      }
-    }
-
-    public enum IndicatorStyle {
-      case `default`
-      case black
-      case white
-    }
-
     public let style: Style?
-    public let alwaysBounceHorizontal: Bool
-    public let alwaysBounceVertical: Bool
-    public let bounces: Bool
-    public let bouncesZoom: Bool
-    public let contentInset: EdgeInsets
-    public let indicatorStyle: IndicatorStyle
-    public let isDirectionalLockEnabled: Bool
-    public let isPagingEnabled: Bool
-    public let isScrollEnabled: Bool
-    public let maximumZoomScale: Float
-    public let minimumZoomScale: Float
-    public let scrollIndicatorInsets: EdgeInsets
-    public let scrollsToTop: Bool
-    public let showsHorizontalScrollIndicator: Bool
-    public let showsVerticalScrollIndicator: Bool
-    public let zoomScale: Float
+    public let scrollOptions: ScrollOptions
 
     public init(
       _ style: Style? = nil,
@@ -60,36 +20,38 @@ public struct ScrollView: HostComponent {
       alwaysBounceVertical: Bool = false,
       bounces: Bool = true,
       bouncesZoom: Bool = true,
-      contentInset: EdgeInsets = EdgeInsets(),
-      indicatorStyle: IndicatorStyle = .default,
+      contentInset: ScrollOptions.EdgeInsets = ScrollOptions.EdgeInsets(),
+      indicatorStyle: ScrollOptions.IndicatorStyle = .default,
       isDirectionalLockEnabled: Bool = false,
       isPagingEnabled: Bool = false,
       isScrollEnabled: Bool = true,
       maximumZoomScale: Float = 1.0,
       minimumZoomScale: Float = 1.0,
-      scrollIndicatorInsets: EdgeInsets = EdgeInsets(),
+      scrollIndicatorInsets: ScrollOptions.EdgeInsets = ScrollOptions.EdgeInsets(),
       scrollsToTop: Bool = true,
       showsHorizontalScrollIndicator: Bool = true,
       showsVerticalScrollIndicator: Bool = true,
       zoomScale: Float = 1.0
     ) {
       self.style = style
-      self.alwaysBounceHorizontal = alwaysBounceHorizontal
-      self.alwaysBounceVertical = alwaysBounceVertical
-      self.bounces = bounces
-      self.bouncesZoom = bouncesZoom
-      self.contentInset = contentInset
-      self.isDirectionalLockEnabled = isDirectionalLockEnabled
-      self.isPagingEnabled = isPagingEnabled
-      self.indicatorStyle = indicatorStyle
-      self.isScrollEnabled = isScrollEnabled
-      self.maximumZoomScale = maximumZoomScale
-      self.minimumZoomScale = minimumZoomScale
-      self.scrollIndicatorInsets = scrollIndicatorInsets
-      self.scrollsToTop = scrollsToTop
-      self.showsVerticalScrollIndicator = showsVerticalScrollIndicator
-      self.showsHorizontalScrollIndicator = showsHorizontalScrollIndicator
-      self.zoomScale = zoomScale
+      scrollOptions = ScrollOptions(
+        alwaysBounceHorizontal: alwaysBounceHorizontal,
+        alwaysBounceVertical: alwaysBounceVertical,
+        bounces: bounces,
+        bouncesZoom: bouncesZoom,
+        contentInset: contentInset,
+        indicatorStyle: indicatorStyle,
+        isDirectionalLockEnabled: isDirectionalLockEnabled,
+        isPagingEnabled: isPagingEnabled,
+        isScrollEnabled: isScrollEnabled,
+        maximumZoomScale: maximumZoomScale,
+        minimumZoomScale: minimumZoomScale,
+        scrollIndicatorInsets: scrollIndicatorInsets,
+        scrollsToTop: scrollsToTop,
+        showsHorizontalScrollIndicator: showsHorizontalScrollIndicator,
+        showsVerticalScrollIndicator: showsVerticalScrollIndicator,
+        zoomScale: zoomScale
+      )
     }
   }
 
--- Sources/TokamakUIKit/Components/Host/ScrollView.swift
+++ Sources/TokamakUIKit/Components/Host/ScrollView.swift
@@ -14,30 +14,6 @@ final class TokamakScrollView: UIScrollView, Default {
   }
 }
 
-extension UIEdgeInsets {
-  public init(_ edges: ScrollView.Props.EdgeInsets) {
-    self.init(
-      top: CGFloat(edges.top),
-      left: CGFloat(edges.left),
-      bottom: CGFloat(edges.bottom),
-      right: CGFloat(edges.right)
-    )
-  }
-}
-
-extension UIScrollView.IndicatorStyle {
-  public init(_ type: ScrollView.Props.IndicatorStyle) {
-    switch type {
-    case .default:
-      self = .default
-    case .black:
-      self = .black
-    case .white:
-      self = .white
-    }
-  }
-}
-
 extension ScrollView: UIViewComponent {
   public typealias RefTarget = UIScrollView
 
@@ -46,23 +22,7 @@ extension ScrollView: UIViewComponent {
     _ props: ScrollView.Props,
     _ children: AnyNode
   ) {
-    let view = box.view
-    view.alwaysBounceHorizontal = props.alwaysBounceHorizontal
-    view.alwaysBounceVertical = props.alwaysBounceVertical
-    view.bounces = props.bounces
-    view.bouncesZoom = props.bouncesZoom
-    view.contentInset = UIEdgeInsets(props.contentInset)
-    view.isDirectionalLockEnabled = props.isDirectionalLockEnabled
-    view.isPagingEnabled = props.isPagingEnabled
-    view.indicatorStyle = UIScrollView.IndicatorStyle(props.indicatorStyle)
-    view.isScrollEnabled = props.isScrollEnabled
-    view.maximumZoomScale = CGFloat(props.maximumZoomScale)
-    view.minimumZoomScale = CGFloat(props.minimumZoomScale)
-    view.scrollIndicatorInsets = UIEdgeInsets(props.scrollIndicatorInsets)
-    view.scrollsToTop = props.scrollsToTop
-    view.showsVerticalScrollIndicator = props.showsVerticalScrollIndicator
-    view.showsHorizontalScrollIndicator = props.showsHorizontalScrollIndicator
-    view.zoomScale = CGFloat(props.zoomScale)
+    applyScrollOptions(box, props.scrollOptions)
   }
 
   static func unmount(
