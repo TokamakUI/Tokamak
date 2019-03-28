3555dcd1f3fd4777127197d0c3adf7867d0d693d
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-03-28 13:02:06 +0200

    Refactor TextViewExample

--- Example/Tokamak/Components/TextViewExample.swift
+++ Example/Tokamak/Components/TextViewExample.swift
@@ -13,59 +13,59 @@ struct TextViewExample: LeafComponent {
   typealias Props = Null
 
   static func render(props: Props, hooks: Hooks) -> AnyNode {
+    let alignment = hooks.state(0)
+
+    let cases = TextAlignment.allCases
+
     return StackView.node(.init(
       Edges.equal(to: .safeArea),
       axis: .vertical,
-      distribution: .fillEqually
+      distribution: .fill,
+      spacing: 10
     ), [
-      TextView.node(.init(text: ExampleText, textAlignment: .right)),
+      SegmentedControl.node(
+        .init(
+          value: alignment.value,
+          valueHandler: Handler { alignment.set($0) }
+        ),
+        cases.map { String(describing: $0) }
+      ),
+      TextView.node(.init(
+        text: exampleText,
+        textAlignment: cases[alignment.value]
+      )),
     ])
   }
 }
 
-let ExampleText = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nam rhoncus eros eros,
-id laoreet libero vehicula posuere.Etiam sit amet auctor mauris.In bibendum
-rhoncus tincidunt.Nulla facilisis sagittis tellus, vitae tempus elit interdum
-eget.Nam varius, velit ut condimentum suscipit, erat nunc molestie velit, a
-feugiat metus ante sit amet quam.Phasellus lobortis risus ac urna sagittis
-congue.Mauris vel eros lacus.Praesent sed feugiat felis, at ullamcorper
-massa.Integer vitae iaculis turpis, in dapibus nunc.Fusce maximus fermentum
-eros sed pellentesque.Mauris sed diam sed justo elementum iaculis non dignissim
+let exampleText = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus eros eros,\
+id laoreet libero vehicula posuere. Etiam sit amet auctor mauris. In bibendum\
+rhoncus tincidunt. Nulla facilisis sagittis tellus, vitae tempus elit interdum\
+eget. Nam varius, velit ut condimentum suscipit, erat nunc molestie velit, a\
+feugiat metus ante sit amet quam. Phasellus lobortis risus ac urna sagittis\
+congue. Mauris vel eros lacus. Praesent sed feugiat felis, at ullamcorper\
+massa. Integer vitae iaculis turpis, in dapibus nunc. Fusce maximus fermentum\
+eros sed pellentesque. Mauris sed diam sed justo elementum iaculis non dignissi
+erat.\
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus eros eros,\
+id laoreet libero vehicula posuere. Etiam sit amet auctor mauris. In bibendum\
+rhoncus tincidunt. Nulla facilisis sagittis tellus, vitae tempus elit interdum\
+eget. Nam varius, velit ut condimentum suscipit, erat nunc molestie velit, a\
+feugiat metus ante sit amet quam. Phasellus lobortis risus ac urna sagittis\
+congue. Mauris vel eros lacus. Praesent sed feugiat felis, at ullamcorper\
+massa. Integer vitae iaculis turpis, in dapibus nunc. Fusce maximus fermentum\
+eros sed pellentesque. Mauris sed diam sed justo elementum iaculis non dignissi
+erat.\
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam rhoncus eros eros,\
+id laoreet libero vehicula posuere. Etiam sit amet auctor mauris. In bibendum\
+rhoncus tincidunt. Nulla facilisis sagittis tellus, vitae tempus elit interdum\
+eget. Nam varius, velit ut condimentum suscipit, erat nunc molestie velit, a\
+feugiat metus ante sit amet quam. Phasellus lobortis risus ac urna sagittis\
+congue. Mauris vel eros lacus. Praesent sed feugiat felis, at ullamcorper\
+massa. Integer vitae iaculis turpis, in dapibus nunc. Fusce maximus fermentum\
+eros sed pellentesque. Mauris sed diam sed justo elementum iaculis non dignissi\
 erat.
-
-Vivamus aliquam rutrum elementum.In at nulla ut enim vestibulum
-gravida.Curabitur porttitor lorem eu risus venenatis iaculis.Curabitur vel
-euismod nibh.Donec ante nisl, sagittis ac magna a, placerat aliquet
-tortor.Vestibulum id tellus ut nulla egestas posuere.Ut non quam sed tellus
-tempor elementum ac eu tellus.Ut metus tellus, molestie sit amet cursus sed,
-fringilla vel erat.
-
-Phasellus ultrices, magna eget eleifend molestie, sem odio congue tellus,
-lobortis egestas orci lorem sit amet purus.Maecenas sed ante hendrerit, sodales
-ligula non, iaculis augue.Ut sed aliquam metus, in malesuada arcu.Morbi tempor
-lorem vitae turpis vehicula finibus.Fusce a rhoncus ipsum.Integer fermentum
-lectus in erat facilisis, id imperdiet purus congue.Vestibulum egestas porta
-tristique.Nulla at gravida dolor.Pellentesque id malesuada neque, elementum
-hendrerit magna.Praesent at odio non magna sagittis porta.Vivamus tempus lacus
-ac sem dignissim, sed cursus tortor suscipit.Nam laoreet et est non mollis.
-
-Mauris mollis, lacus a bibendum venenatis, quam lectus ultrices turpis,
-dignissim feugiat erat nisl non metus.Aliquam a ex semper, egestas velit non,
-sagittis odio.Integer pharetra molestie turpis dictum fringilla.Vivamus
-sollicitudin porta quam et porta.Nam lobortis tortor vel lectus accumsan, vel
-commodo lorem fringilla.Orci varius natoque penatibus et magnis dis parturient
-montes, nascetur ridiculus mus.Praesent porta, metus vitae fringilla dignissim,
-nisl nisi ultrices arcu, nec volutpat quam nisi vitae elit.Donec vel nisl
-vulputate, scelerisque ipsum ut, tempus erat.
-
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-inceptos himenaeos.Vestibulum ut turpis in eros scelerisque molestie.Ut aliquet
-mauris lacinia mauris pretium placerat.Duis tempus nulla lectus, vitae eleifend
-turpis semper eget.Integer in convallis odio.Fusce maximus consectetur
-pharetra.Duis pharetra odio id neque mattis, ac aliquam velit dapibus.Ut
-aliquam nisi vitae tortor varius fermentum eu ut enim.Nulla placerat erat vitae
-diam scelerisque convallis.Sed sollicitudin commodo justo, ut ullamcorper orci
-vulputate at.Proin aliquam, turpis nec euismod semper, dolor nibh rhoncus nibh,
-quis condimentum eros purus eget ligula.
 """
