6d8e9283bce9fe1a1f25d2cb091ae60be8490a66
Max Desiatov <max@desiatov.com>
2019-03-31 13:18:06 +0100

    Add build jobs/stages to .travis.yml

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to
 // build this package.
 
@@ -6,10 +6,6 @@ import PackageDescription
 
 let package = Package(
   name: "Tokamak",
-  platforms: [
-    .macOS(.v10_14),
-    .iOS(.v11),
-  ],
   products: [
     // Products define the executables and libraries produced by a package,
     // and make them visible to other packages.
