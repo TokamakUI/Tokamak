c98bcd0e67491904c3cc1bbaf60fcc3b42dc1970
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-04-08 15:40:06 +0300

    Remove unused LintError case

--- Sources/TokamakLint/LintError.swift
+++ Sources/TokamakLint/LintError.swift
@@ -8,13 +8,10 @@
 import Foundation
 
 public enum LintError: Error, CustomStringConvertible {
-  case hasntTokamakImport
   case propsIsNotEquatable
 
   public var description: String {
     switch self {
-    case .hasntTokamakImport:
-      return "Hasn't Tokamak import"
     case .propsIsNotEquatable:
       return "Props is not Equatable"
     }
