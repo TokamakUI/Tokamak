532918984c9e43cef82ae277238216565e483eac
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-05-08 18:28:07 +0300

    Format comments

--- Sources/TokamakLint/Helpers/GetRender.swift
+++ Sources/TokamakLint/Helpers/GetRender.swift
@@ -13,34 +13,27 @@ extension Array: Error where Element == StyleViolation {}
 func getRender(from node: Node, at file: String) throws -> Node {
   let renders = node.getNodes(with: "render").filter {
     // check if render type if function
-
     var memberDeclListItem: Node = $0
-
     while memberDeclListItem.text != "MemberDeclListItem"
       && memberDeclListItem.parent != nil {
       guard let parent = memberDeclListItem.parent else { break }
       memberDeclListItem = parent
     }
-
     guard memberDeclListItem.children[0].text
       == TokenDeclType.function.rawValue else { return false }
 
     // check if render is static
-
     let staticModifier = memberDeclListItem.getNodes(
       with: "DeclModifier"
     ).filter { (modifier) -> Bool in
       modifier.children[0].text == "static"
     }
-
     guard staticModifier.first != nil else { return false }
 
     // check if render is on first layer of component
-
     let firstLayerCildrens = memberDeclListItem.children[0].children.map {
       $0.text
     }
-
     guard firstLayerCildrens.contains("render") else { return false }
 
     return true
