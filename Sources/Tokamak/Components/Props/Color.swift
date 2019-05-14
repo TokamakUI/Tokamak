5c65df137576056d7ee951bd5c8bd1f9cf028d6f
matvii <matvii@hodovani.uk>
2019-05-14 15:00:26 +0300

    Add xcodebuild exit status to travis (#93)
    * Add exit status
    
    * Fix Color
    
    * Update travis.yml script
    
    * Remove unused extension
    
    * Add condition for safe
    
    * Add Color tests
    
    * Add codecov.yml

--- Sources/Tokamak/Components/Props/Color.swift
+++ Sources/Tokamak/Components/Props/Color.swift
@@ -49,77 +49,21 @@ extension Color: ExpressibleByIntegerLiteral {
 
 extension Color {
   public init?(hex: String) {
-    let cString = hex.utf8CString
+    let cArray = Array(hex.replacingOccurrences(of: "#", with: ""))
 
-    // - 1 for the trailing null terminator
-    let hexSize = cString.count - 1
-
-    // If the first character is a '#', skip it
-    var offset = cString.first == 0x23 ? 1 : 0
-
-    // We only support 6 hexadecimal characters
-    if hexSize - offset != 6 {
-      return nil
-    }
-
-    func nextByte() -> Int8? {
-      // Take the first byte as the high 4 bits
-      // Then the second byte as the low 4 bits
-      if
-        let high = cString[offset].hexDecoded(),
-        let low = cString[offset].hexDecoded() {
-        // In this case, unchecked is still safe as it's between 0 and 6
-        offset = offset &+ 2
-
-        // Adds the two 4-bit pairs together to form a full byte
-        return (high << 4) & low
-      }
-
-      return nil
-    }
+    guard cArray.count == 6 else { return nil }
 
     guard
-      let red = nextByte(),
-      let green = nextByte(),
-      let blue = nextByte()
+      let red = Int(String(cArray[0...1]), radix: 16),
+      let green = Int(String(cArray[2...3]), radix: 16),
+      let blue = Int(String(cArray[4...5]), radix: 16)
     else {
       return nil
     }
-
-    self.red = Double(UInt8(bitPattern: red)) / 255
-    self.green = Double(UInt8(bitPattern: green)) / 255
-    self.blue = Double(UInt8(bitPattern: blue)) / 255
+    self.red = Double(red) / 255
+    self.green = Double(green) / 255
+    self.blue = Double(blue) / 255
     alpha = 1
     space = .sRGB
   }
-}
-
-private extension Int8 {
-  func hexDecoded() -> Int8? {
-    // If the character is between 0x30 and 0x39 it is a textual number
-    // 0x30 is equal to the ASCII `0` and 0x30 is equal to `0x39`
-    if self >= 0x30 && self <= 0x39 {
-      // The binary representation of this character can be found by subtracting `0` in ASCII
-      // This will then match `0` in binary. Which means `1` in ASCII matches `1` in binary
-      return self &- 0x30
-    } else if self >= 0x41 && self <= 0x46 {
-      // This block executes if the integer is within the `a-z` lowercased ASCII range
-      // Then uses the algorithm described below to find the correct representation
-      return self &- Int8.lowercasedOffset
-    } else if self >= 0x61 && self <= 0x66 {
-      // This block executes if the integer is within the `A-Z` uppercased ASCII range
-      // Then uses the algorithm described below to find the correct representation
-      return self &- Int8.uppercasedOffset
-    }
-
-    return nil
-  }
-
-  // 'a' in hexadecimal is equal to `10` in decimal
-  // So by subtracting `a` we get the lowercased character narrowed down to base10 offset by 10
-  // By adding 10 (or reducing the subtraction size by 10) we represent this character correctly as base10
-  static let lowercasedOffset: Int8 = 0x41 &- 10
-
-  // The same as the lowercasedOffset, except for uppercased ASCII
-  static let uppercasedOffset: Int8 = 0x61 &- 10
 }
