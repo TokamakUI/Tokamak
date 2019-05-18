9ee7319ef855ea497916b5d7bdaac610f00cbf72
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-05-18 13:51:06 +0300

    Move getRender to Node extension

--- Sources/TokamakLint/Helpers/GetRender.swift
+++ Sources/TokamakLint/Helpers/GetRender.swift
@@ -10,56 +10,58 @@ import SwiftSyntax
 
 extension Array: Error where Element == StyleViolation {}
 
-func getRender(from node: Node, at file: String) throws -> Node {
-  let renders = node.children(with: "render").filter {
-    // check if render type is function
-    var memberDeclListItem = $0
-    while memberDeclListItem.text != SyntaxKind.memberDeclListItem.rawValue
-      && memberDeclListItem.parent != nil {
-      guard let parent = memberDeclListItem.parent else { break }
-      memberDeclListItem = parent
+extension Node {
+  func getRender(at file: String) throws -> Node {
+    let renders = self.children(with: "render").filter {
+      // check if render type is function
+      var memberDeclListItem = $0
+      while memberDeclListItem.text != SyntaxKind.memberDeclListItem.rawValue
+        && memberDeclListItem.parent != nil {
+        guard let parent = memberDeclListItem.parent else { break }
+        memberDeclListItem = parent
+      }
+      guard let functionDecl = memberDeclListItem.children.first,
+        functionDecl.text == SyntaxKind.functionDecl.rawValue else { return false }
+
+      // check if render is static
+      let staticModifier = memberDeclListItem.children(
+        with: SyntaxKind.declModifier.rawValue
+      ).filter {
+        guard let child = $0.children.first else { return false }
+        return child.text == "static"
+      }
+      guard staticModifier.first != nil else { return false }
+
+      // check if render is on first layer of component
+      return functionDecl.children.map {
+        $0.text
+      }.contains("render")
     }
-    guard let functionDecl = memberDeclListItem.children.first,
-      functionDecl.text == SyntaxKind.functionDecl.rawValue else { return false }
 
-    // check if render is static
-    let staticModifier = memberDeclListItem.children(
-      with: SyntaxKind.declModifier.rawValue
-    ).filter {
-      guard let child = $0.children.first else { return false }
-      return child.text == "static"
-    }
-    guard staticModifier.first != nil else { return false }
-
-    // check if render is on first layer of component
-    return functionDecl.children.map {
-      $0.text
-    }.contains("render")
-  }
-
-  guard renders.count == 1 else {
-    if renders.count > 1 {
-      throw renders.map {
-        StyleViolation(
+    guard renders.count == 1 else {
+      if renders.count > 1 {
+        throw renders.map {
+          StyleViolation(
+            ruleDescription: OneRenderFunctionRule.description,
+            location: Location(
+              file: file,
+              line: $0.range.startRow,
+              character: $0.range.startColumn
+            )
+          )
+        }
+      } else {
+        throw [StyleViolation(
           ruleDescription: OneRenderFunctionRule.description,
           location: Location(
             file: file,
-            line: $0.range.startRow,
-            character: $0.range.startColumn
+            line: self.range.startRow,
+            character: self.range.startColumn
           )
-        )
+        )]
       }
-    } else {
-      throw [StyleViolation(
-        ruleDescription: OneRenderFunctionRule.description,
-        location: Location(
-          file: file,
-          line: node.range.startRow,
-          character: node.range.startColumn
-        )
-      )]
     }
-  }
 
-  return renders[0]
+    return renders[0]
+  }
 }
