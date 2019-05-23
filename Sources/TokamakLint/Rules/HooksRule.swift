484dd7463b8c7182d34748e7755293c8bc617f56
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-05-23 15:56:01 +0300

    Refactor HooksRule

--- Sources/TokamakLint/Rules/HooksRule.swift
+++ Sources/TokamakLint/Rules/HooksRule.swift
@@ -11,69 +11,67 @@ struct HooksRule: Rule {
   static let description = RuleDescription(
     type: HooksRule.self,
     name: "Rules of Hooks",
-    description: "Code need to follow rules of hooks"
+    description: "Use hooks on top level of render"
   )
 
   public static func validate(visitor: TokenVisitor) -> [StyleViolation] {
+    var violations: [StyleViolation] = []
+
     // search for render function
-    do {
-      let structs = visitor.root.children(with: "struct")
-        .compactMap { $0.firstParent(of: SyntaxKind.structDecl.rawValue) }
-        .filter { structDecl in
-          let hookedProtocols = ["CompositeComponent", "LeafComponent"]
-          guard let typeInheritanceClause = structDecl.firstChild(
-            of: SyntaxKind.typeInheritanceClause.rawValue
-          ) else { return false }
-          let types = typeInheritanceClause.children(
-            with: SyntaxKind.simpleTypeIdentifier.rawValue
-          ).compactMap { $0.children.first?.text }
-          return types.contains { hookedProtocols.contains($0) }
-        }
+    let structs = visitor.root.children(with: "struct")
+      .compactMap { $0.firstParent(of: SyntaxKind.structDecl.rawValue) }
+      .filter { structDecl in
+        let hookedProtocols = ["CompositeComponent", "LeafComponent"]
+        guard let typeInheritanceClause = structDecl.firstChild(
+          of: SyntaxKind.typeInheritanceClause.rawValue
+        ) else { return false }
+        let types = typeInheritanceClause.children(
+          with: SyntaxKind.simpleTypeIdentifier.rawValue
+        ).compactMap { $0.children.first?.text }
+        return types.contains { hookedProtocols.contains($0) }
+      }
+    guard !structs.isEmpty else { return [] }
+    structs.forEach { structDecl in
+      for render in structDecl.children(with: "render") {
+        // search for Hooks argument name in render argument list
+        let renderFuncDecl = render.firstParent(of: .functionDecl)
+        let funcSign = renderFuncDecl?.firstChild(of: .functionSignature)
+        let funcParameterList = funcSign?
+          .firstChild(of: .functionParameterList)
+        let hooksParameter = funcParameterList?.firstChild(of: "Hooks")?
+          .firstParent(of: .functionParameter)
+        guard let hooksName = hooksParameter?.children.first?.text
+        else { return }
 
-      guard !structs.isEmpty else { return [] }
+        // search for all hooks in code
+        guard let hooks = renderFuncDecl?.children(with: hooksName)
+        else { return }
 
-      var violations: [StyleViolation] = []
-
-      try structs.forEach { structDecl in
-        do {
-          let render = try structDecl.getOneRender(at: visitor.path)
-
-          // search for Hooks argument name in render argument list
-          guard let renderFuncDecl = render.firstParent(of: .functionDecl) else { throw [] }
-          let funcSign = renderFuncDecl.firstChild(of: .functionSignature)
-          let funcParameterList = funcSign?.firstChild(of: .functionParameterList)
-          let hooksParameter = funcParameterList?.firstChild(of: "Hooks")?.firstParent(of: .functionParameter)
-          guard let hooksName = hooksParameter?.children.first?.text else { throw [] }
-
-          // search for all hooks in code
-          let hooks = renderFuncDecl.children(with: hooksName)
-
-          // check that Hooks.state is on the first layer of render function
-          guard let codeBlockItemList = renderFuncDecl.firstChild(of: .codeBlockItemList) else { throw [] }
-
-          throw hooks.compactMap { (hook) -> StyleViolation? in
-            guard let hookMemberAccessExpr = hook.firstParent(of: .memberAccessExpr) else { return nil }
-            guard let hookCodeBlockItem = hookMemberAccessExpr.firstParent(of: .codeBlockItem) else { return nil }
-            guard codeBlockItemList.children.contains(hookCodeBlockItem) else { return StyleViolation(
-              ruleDescription: OneRenderFunctionRule.description,
-              location: Location(
-                file: visitor.path,
-                line: hook.range.startRow,
-                character: hook.range.startColumn
-              )
-            ) }
-            return nil
-          }
-        } catch let error as [StyleViolation] {
-          violations.append(contentsOf: error)
-        } catch {
-          throw error
+        // check that Hooks.state is on the first layer of render function
+        guard let codeBlockItemList = renderFuncDecl?
+          .firstChild(of: .codeBlockItemList)
+        else { return }
+        hooks.forEach { hook in
+          guard let hookMemberAccessExpr = hook
+            .firstParent(of: .memberAccessExpr)
+          else { return }
+          guard let hookCodeBlockItem = hookMemberAccessExpr
+            .firstParent(of: .codeBlockItem)
+          else { return }
+          guard !codeBlockItemList.children.contains(hookCodeBlockItem)
+          else { return }
+          violations.append(StyleViolation(
+            ruleDescription: OneRenderFunctionRule.description,
+            location: Location(
+              file: visitor.path,
+              line: hook.range.startRow,
+              character: hook.range.startColumn
+            )
+          ))
         }
       }
-      return violations
-    } catch {
-      print(error)
     }
-    return []
+
+    return violations
   }
 }
