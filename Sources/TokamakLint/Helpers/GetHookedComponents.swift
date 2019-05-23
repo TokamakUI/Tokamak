3e767a72e472880cb116784469bf08831c2af821
Hodovaniuk Matvii <matvii@hodovani.uk>
2019-05-23 17:24:34 +0300

    Apply swiftformat

--- Sources/TokamakLint/Helpers/GetHookedComponents.swift
+++ Sources/TokamakLint/Helpers/GetHookedComponents.swift
@@ -8,21 +8,20 @@
 import SwiftSyntax
 
 extension Node {
-
-    // func to return Tokamak components that can have hooks in render
-
-    var hookedComponents: [Node] {
-        return children(with: "struct")
-            .compactMap { $0.firstParent(of: SyntaxKind.structDecl.rawValue) }
-            .filter { structDecl in
-                let hookedProtocols = ["CompositeComponent", "LeafComponent"]
-                guard let typeInheritanceClause = structDecl.firstChild(
-                    of: SyntaxKind.typeInheritanceClause.rawValue
-                    ) else { return false }
-                let types = typeInheritanceClause.children(
-                    with: SyntaxKind.simpleTypeIdentifier.rawValue
-                    ).compactMap { $0.children.first?.text }
-                return types.contains { hookedProtocols.contains($0) }
-        }
-    }
+  /// return Tokamak components that can have hooks in the render
+  /// placed as a children of node
+  var hookedComponents: [Node] {
+    return children(with: "struct")
+      .compactMap { $0.firstParent(of: SyntaxKind.structDecl.rawValue) }
+      .filter { structDecl in
+        let hookedProtocols = ["CompositeComponent", "LeafComponent"]
+        guard let typeInheritanceClause = structDecl.firstChild(
+          of: SyntaxKind.typeInheritanceClause.rawValue
+        ) else { return false }
+        let types = typeInheritanceClause.children(
+          with: SyntaxKind.simpleTypeIdentifier.rawValue
+        ).compactMap { $0.children.first?.text }
+        return types.contains { hookedProtocols.contains($0) }
+      }
+  }
 }
