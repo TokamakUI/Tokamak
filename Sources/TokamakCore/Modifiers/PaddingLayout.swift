4b3b377c822eee6773201913a9c4ddf1fb8f9682
Carson Katri <Carson.katri@gmail.com>
2020-06-29 10:33:27 -0400

    Add layout modifiers (#118)
    * frame and padding modifiers

---
+++ Sources/TokamakCore/Modifiers/PaddingLayout.swift
@@ -0,0 +1,42 @@
+// Copyright 2020 Tokamak contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+public struct _PaddingLayout: ViewModifier {
+  public var edges: Edge.Set
+  public var insets: EdgeInsets?
+
+  public init(edges: Edge.Set = .all, insets: EdgeInsets?) {
+    self.edges = edges
+    self.insets = insets
+  }
+
+  public func body(content: Content) -> some View {
+    content
+  }
+}
+
+extension View {
+  public func padding(_ insets: EdgeInsets) -> some View {
+    modifier(_PaddingLayout(insets: insets))
+  }
+
+  public func padding(_ edges: Edge.Set = .all, _ length: CGFloat? = nil) -> some View {
+    let insets = length.map { EdgeInsets(_all: $0) }
+    return modifier(_PaddingLayout(edges: edges, insets: insets))
+  }
+
+  public func padding(_ length: CGFloat) -> some View {
+    padding(.all, length)
+  }
+}
--- Sources/TokamakDemo/SVGCircle.swift
+++ Sources/TokamakDemo/SVGCircle.swift
@@ -16,9 +16,9 @@ import TokamakDOM
 
 struct SVGCircle: View {
   var body: some View {
-    HTML("svg", ["width": "100", "height": "100"]) {
+    HTML("svg", ["width": "100%", "height": "100%"]) {
       HTML("circle", [
-        "cx": "50", "cy": "50", "r": "40",
+        "cx": "50%", "cy": "50%", "r": "40%",
         "stroke": "green", "stroke-width": "4", "fill": "yellow",
       ])
     }
