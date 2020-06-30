e94e976da5fee1bb772ea200fda587132ef45c45
Carson Katri <carson.katri@gmail.com>
2020-06-30 11:16:15 -0400

    Add ScrollView

--- Sources/TokamakDOM/DOMRenderer.swift
+++ Sources/TokamakDOM/DOMRenderer.swift
@@ -57,7 +57,7 @@ public final class DOMRenderer: Renderer {
 
   public init<V: View>(_ view: V, _ ref: JSObjectRef) {
     rootRef = ref
-    rootRef.style = "display: flex; width: 100%; height: 100%; justify-content: center; align-items: center;"
+    rootRef.style = "display: flex; width: 100%; height: 100%; justify-content: center; align-items: center; overflow: hidden;"
     reconciler = StackReconciler(
       view: view,
       target: DOMNode(view, ref),
---
+++ Sources/TokamakDOM/Views/ScrollView.swift
@@ -0,0 +1,46 @@
+// Copyright 2020 Tokamak contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//  Created by Carson Katri on 06/29/2020.
+//
+
+import TokamakCore
+
+public typealias ScrollView = TokamakCore.ScrollView
+
+extension ScrollView: ViewDeferredToRenderer, SpacerContainer {
+  var axis: SpacerContainerAxis {
+    if axes.contains(.horizontal) {
+      return .horizontal
+    } else {
+      return .vertical
+    }
+  }
+
+  public var deferredBody: AnyView {
+    let scrollX = axes.contains(.horizontal)
+    let scrollY = axes.contains(.vertical)
+    // FIXME: I can only hide the scrollbars on Firefox
+    // Safari/Chrome require a `::-webkit-scrollbar` selector, which can't be done with inline styles
+    return AnyView(HTML("div", ["style": """
+    \(scrollX ? "overflow-x: auto; width: 100%;" : "overflow-x: hidden;")
+    \(scrollY ? "overflow-y: auto; height: 100%;" : "overflow-y: hidden;")
+    \(!showsIndicators ? "scrollbar-color: transparent;" : "")
+    \(fillCrossAxis && scrollX ? "height: 100%;" : "")
+    \(fillCrossAxis && scrollY ? "width: 100%;" : "")
+    """]) {
+      content
+    })
+  }
+}
