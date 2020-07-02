ebcac6db70b2200977bd0c0d3f44153460511869
Max Desiatov <max@desiatov.com>
2020-07-02 23:21:33 +0100

    Add Group implementation (#150)

---
+++ Sources/TokamakCore/Views/Group.swift
@@ -0,0 +1,33 @@
+// Copyright 2020 Tokamak contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+public struct Group<Content> {
+  let content: Content
+}
+
+extension Group: View where Content: View {
+  public init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+
+  public var body: Never {
+    neverBody("Group")
+  }
+}
+
+extension Group: ParentView where Content: View {
+  public var children: [AnyView] { (content as? ParentView)?.children ?? [AnyView(content)] }
+}
+
+extension Group: GroupView where Content: View {}
