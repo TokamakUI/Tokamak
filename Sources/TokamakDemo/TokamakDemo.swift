83a9c3fdbf03ff26191dcfd37faebd08043f025c
Jed Fox <git@jedfox.com>
2020-07-06 18:53:04 -0400

    Fixes

--- Sources/TokamakDemo/TokamakDemo.swift
+++ Sources/TokamakDemo/TokamakDemo.swift
@@ -21,47 +21,87 @@ import SwiftUI
 import TokamakDOM
 #endif
 
+struct NavItem: Identifiable {
+  var id: String
+  var destination: AnyView
+
+  init<V>(_ id: String, destination: V) where V: View {
+    self.id = id
+    self.destination = AnyView(
+      destination
+        .frame(minWidth: 300)
+        .navigationBarTitle(id)
+    )
+  }
+}
+
+var outlineGroupDemo: AnyView {
+  if #available(OSX 10.16, iOS 14.0, *) {
+    return AnyView(OutlineGroupDemo())
+  } else {
+    return AnyView(Text("OutlineGroup Unavailable"))
+  }
+}
+
+#if canImport(TokamakDOM)
+var svgDemo = SVGCircle()
+#else
+var svgDemo = Text("SVG is unavailable")
+#endif
+
+#if !os(macOS)
+var listDemo: AnyView {
+  if #available(iOS 14.0, *) {
+    return AnyView(ListDemo().listStyle(InsetGroupedListStyle()))
+  } else {
+    return AnyView(ListDemo())
+  }
+}
+#else
+var listDemo = ListDemo()
+#endif
+
+var links: [NavItem] {
+  [
+    NavItem("Counter", destination:
+      Counter(count: 5, limit: 15)
+        .padding()
+        .background(Color(red: 0.9, green: 0.9, blue: 0.9, opacity: 1.0))
+        .border(Color.red, width: 3)),
+    NavItem("ZStack", destination: ZStack {
+      Text("I'm on bottom")
+      Text("I'm forced to the top")
+        .zIndex(1)
+      Text("I'm on top")
+    }.padding(20)),
+    NavItem("ForEach", destination: ForEachDemo()),
+    NavItem("Text", destination: TextDemo()),
+    NavItem("SVG", destination: svgDemo),
+    NavItem("TextField", destination: TextFieldDemo()),
+    NavItem("Spacer", destination: SpacerDemo()),
+    NavItem("Environment", destination: EnvironmentDemo().font(.system(size: 8))),
+    NavItem("List", destination: listDemo),
+    NavItem("OutlineGroup", destination: outlineGroupDemo),
+  ]
+}
+
 struct TokamakDemoView: View {
   var body: some View {
-    NavigationView {
-      VStack(alignment: .leading, spacing: 16) {
-        Spacer()
-        Group {
-          NavigationLink("Counter", destination:
-            Counter(count: 5, limit: 15)
-              .padding()
-              .background(Color(red: 0.9, green: 0.9, blue: 0.9, opacity: 1.0))
-              .border(Color.red, width: 3))
-
-          NavigationLink("ZStack", destination: ZStack {
-            Text("I'm on bottom")
-            Text("I'm forced to the top")
-              .zIndex(1)
-            Text("I'm on top")
-          }.padding(20))
-          NavigationLink("ForEach", destination: ForEachDemo())
-          NavigationLink("Text", destination: TextDemo())
-          #if canImport(TokamakDOM)
-          NavigationLink("SVG", destination: SVGCircle())
-          #endif
-          NavigationLink("TextField", destination: TextFieldDemo())
-          NavigationLink("Spacer", destination: SpacerDemo())
-          NavigationLink("Environment", destination: EnvironmentDemo().font(.system(size: 8)))
-        }
-        #if !os(macOS)
-        if #available(iOS 14.0, *) {
-          NavigationLink("List", destination: ListDemo().listStyle(InsetGroupedListStyle()))
-        } else {
-          NavigationLink("List", destination: ListDemo())
-        }
-        #else
-        NavigationLink("List", destination: ListDemo())
-        #endif
-        if #available(OSX 10.16, iOS 14.0, *) {
-          NavigationLink("OutlineGroup", destination: OutlineGroupDemo())
-        }
-        Spacer()
-      }.padding()
+    NavigationView { () -> AnyView in
+      let list = List(links) { link in
+        NavigationLink(link.id, destination: link.destination)
+      }
+      .frame(minHeight: 300)
+      .navigationBarTitle("Demos")
+      #if os(WASI)
+      return AnyView(list)
+      #else
+      if #available(iOS 14.0, *) {
+        return AnyView(list.listStyle(SidebarListStyle()))
+      } else {
+        return AnyView(list)
+      }
+      #endif
     }
   }
 }
