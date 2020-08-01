61632b8501e9795f21ad946d2f83af675db5f80b
Max Desiatov <max@desiatov.com>
2020-08-01 21:22:41 +0100

    Use `import CombineShim` in the observers code

--- Sources/TokamakDOM/App/ColorSchemeObserver.swift
+++ Sources/TokamakDOM/App/ColorSchemeObserver.swift
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import CombineShim
 import JavaScriptKit
-import OpenCombine
 
 enum ColorSchemeObserver {
   static var publisher = CurrentValueSubject<ColorScheme, Never>(
