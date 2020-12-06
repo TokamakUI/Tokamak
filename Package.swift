f76e1d30db6b237074aa4ec6ebd3b221f77748c1
Max Desiatov <max@desiatov.com>
2020-12-06 21:40:35 +0000

    Add JavaScriptKit as a dependency of test target

--- Package.swift
+++ Package.swift
@@ -106,7 +106,14 @@ let package = Package(
     ),
     .target(
       name: "TokamakTestRenderer",
-      dependencies: ["TokamakCore"]
+      dependencies: [
+        "TokamakCore",
+        .product(
+          name: "JavaScriptKit",
+          package: "JavaScriptKit",
+          condition: .when(platforms: [.wasi])
+        ),
+      ]
     ),
     .testTarget(
       name: "TokamakTests",
