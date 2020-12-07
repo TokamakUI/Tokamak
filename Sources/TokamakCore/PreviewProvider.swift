1c0fcb3f873cffed5be2f31a2084fd351528e819
Max Desiatov <max@desiatov.com>
2020-12-07 16:12:36 +0000

    Add missing preview-related types and modifiers

--- Sources/TokamakCore/PreviewProvider.swift
+++ Sources/TokamakCore/PreviewProvider.swift
@@ -18,3 +18,47 @@ public protocol PreviewProvider {
 
   @ViewBuilder static var previews: Previews { get }
 }
+
+public struct PreviewDevice: RawRepresentable, ExpressibleByStringLiteral {
+  public let rawValue: String
+  public init(rawValue: String) {
+    self.rawValue = rawValue
+  }
+
+  public init(stringLiteral: String) {
+    rawValue = stringLiteral
+  }
+}
+
+public protocol PreviewContextKey {
+  associatedtype Value
+  static var defaultValue: Self.Value { get }
+}
+
+public protocol PreviewContext {
+  subscript<Key>(key: Key.Type) -> Key.Value where Key: PreviewContextKey { get }
+}
+
+public enum PreviewLayout {
+  case device
+  case sizeThatFits
+  case fixed(width: CGFloat, height: CGFloat)
+}
+
+public extension View {
+  func previewDevice(_ value: PreviewDevice?) -> some View {
+    self
+  }
+
+  func previewLayout(_ value: PreviewLayout) -> some View {
+    self
+  }
+
+  func previewDisplayName(_ value: String?) -> some View {
+    self
+  }
+
+  func previewContext<C>(_ value: C) -> some View where C: PreviewContext {
+    self
+  }
+}
