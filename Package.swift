2a3ec539521911f6bf3b9d6a841ea2f8017ebc9e
Morten Bek Ditlevsen <morten@ka-ching.dk>
2021-01-10 12:46:23 +0100

    It's enough only to include gdk-3.0 in pkg-config. I also believe that gdk is installed as part of gtk when using brew or apt

--- Package.swift
+++ Package.swift
@@ -88,17 +88,13 @@ let package = Package(
     ),
     .systemLibrary(
       name: "CGDK",
-      pkgConfig: "gdk-3.0 pangocairo pangoft2 pango gio-unix-2.0 glib-2.0",
+      pkgConfig: "gdk-3.0",
       providers: [
-        .brew(["gtk+3", "glib", "glib-networking", "gobject-introspection"]),
-        .apt([
-              "libgtk-3-dev",
-              "libglib2.0-dev",
-              "glib-networking",
-              "gobject-introspection",
-              "libgirepository1.0-dev"
-        ])
-      ]),
+        .apt(["libgtk+-3.0", "gtk+-3.0"]),
+        // .yum(["gtk3-devel"]),
+        .brew(["gtk+3"]),
+      ]
+    ),
     .target(
       name: "TokamakGTKCHelpers",
       dependencies: ["CGTK"]
