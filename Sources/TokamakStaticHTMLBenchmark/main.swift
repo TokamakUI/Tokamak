7be331f874a62601c0cbf8b899eb616cd196189d
Max Desiatov <max@desiatov.com>
2021-01-20 19:23:46 +0000

    Benchmark `App` and `WindowGroup` rendering

--- Sources/TokamakStaticHTMLBenchmark/main.swift
+++ Sources/TokamakStaticHTMLBenchmark/main.swift
@@ -5,6 +5,18 @@ benchmark("render Text") {
   _ = StaticHTMLRenderer(Text("text"))
 }
 
+struct BenchmarkApp: App {
+  var body: some Scene {
+    WindowGroup("Benchmark") {
+      Text("Hello, World!")
+    }
+  }
+}
+
+benchmark("render App") {
+  _ = StaticHTMLRenderer(BenchmarkApp())
+}
+
 benchmark("render List") {
   _ = StaticHTMLRenderer(List(1..<100) { Text("\($0)") })
 }
