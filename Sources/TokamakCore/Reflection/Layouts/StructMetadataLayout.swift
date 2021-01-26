b9661c47c7435148971ccf8b0cf454f78434b0ed
Max Desiatov <max@desiatov.com>
2021-01-26 21:11:18 +0000

    Update copyright headers

--- Sources/TokamakCore/Reflection/Layouts/StructMetadataLayout.swift
+++ Sources/TokamakCore/Reflection/Layouts/StructMetadataLayout.swift
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2017 Wesley Wickwire
+// Copyright (c) 2017-2021 Wesley Wickwire and Tokamak contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
