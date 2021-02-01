c18cbb3f4150f8edb0f9e81cc3ac5990fdf95c21
Mathew Polzin <matt.polzin@gmail.com>
2021-01-31 16:27:22 -0800

    right, switch coverage gen order to have branch checked out second.

--- cov_diff.sh
+++ cov_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-diff_cov_output="$(diff ./codecov.txt ../codecov-b.txt)"
+diff_cov_output="$(diff ../codecov-main.txt ./codecov.txt)"
 diff_cov_output="${diff_cov_output//'%'/'%25'}"
 diff_cov_output="${diff_cov_output//$'\n'/'%0A'}"
 diff_cov_output="${diff_cov_output//$'\r'/'%0D'}"
