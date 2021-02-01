c7712358aa77dc1c6456f55955e7b698848dd14b
Mathew Polzin <matt.polzin@gmail.com>
2021-01-31 16:49:25 -0800

    switch to git diff

--- cov_diff.sh
+++ cov_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-diff_cov_output="$(diff ../codecov-main.txt ./codecov.txt | sed -n '/^[-=][^-=]/p' )"
+diff_cov_output="$(git diff ../codecov-main.txt ./codecov.txt | sed -E -n '/^(---|\+\+\+|@@)/!p' | tail -n +3)"
 diff_cov_output="\`\`\`diff $diff_cov_output \`\`\`"
 diff_cov_output="${diff_cov_output//'%'/'%25'}"
 diff_cov_output="${diff_cov_output//$'\n'/'%0A'}"
