de5f2d1c5b72b37f8320d807ce76f3c76234f318
Mathew Polzin <matt.polzin@gmail.com>
2021-01-31 17:49:24 -0800

    add note about the new script.

--- cov_diff.sh
+++ cov_diff.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# responsible for apply diff to the results of test coverage
+# from main and the current branch.
+# This script only exists because you cannot count on local variable
+# usage within scripts embedded in the GitHub Action workflow directly.
+
 diff_cov_output="$(git diff ../codecov-main.txt ./codecov.txt | sed -E -n '/^(---|\+\+\+|@@)/!p' | tail -n +3)"
 diff_cov_output="\`\`\`diff $diff_cov_output \`\`\`"
 diff_cov_output="${diff_cov_output//'%'/'%25'}"
