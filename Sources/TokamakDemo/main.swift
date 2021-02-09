e05110825a7da453fd9c75cd93df010ac04a73cc
Max Desiatov <max@desiatov.com>
2021-02-09 16:36:41 +0000

    Remove irrelevant modifiers from reproduction code

--- Sources/TokamakDemo/main.swift
+++ Sources/TokamakDemo/main.swift
@@ -5,20 +5,14 @@ struct TokamakApp: App {
     WindowGroup("Spooky Hanger") {
       NavigationView {
         List {
-          ForEach(["Section"], id: \.self) { section in
-            Section(header: Text(section).font(.headline)) {
-              ForEach(["Item 1"], id: \.self) { childRow in
-                NavigationLink(
-                  destination: Text(childRow).padding([.leading, .trailing])
-                ) {
-                  Text(childRow)
-                }
-                .padding(.leading)
-              }
+          ForEach(["Item 1"], id: \.self) { childRow in
+            NavigationLink(
+              destination: Text(childRow).padding([.leading, .trailing])
+            ) {
+              Text(childRow)
             }
           }
         }
-        .listStyle(SidebarListStyle())
       }
     }
   }
