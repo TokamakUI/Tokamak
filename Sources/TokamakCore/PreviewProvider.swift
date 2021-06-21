0bd6d72982b8bd5a46d54d54904b828ecf9987a1
yonihemi <jonathan@hemi.dev>
2021-06-21 17:03:32 +0800

    Explicitly import CoreFoundation
    This allows TokamakDemo to be built with [latest SwiftWasm 5.4 toolchain](https://github.com/swiftwasm/swift/releases/tag/swift-wasm-5.4-SNAPSHOT-2021-06-17-a) in **debug mode** (compiler still crashes when building for release).
    
    BTW this import could be anywhere in the target, couldn't find a file that felt natural to include it in.

--- Sources/TokamakCore/PreviewProvider.swift
+++ Sources/TokamakCore/PreviewProvider.swift
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import Foundation
+import CoreFoundation
 
 /// This protocol has no functionality currently, and is only provided for compatibility purposes.
 public protocol PreviewProvider {
