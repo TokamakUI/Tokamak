e6c37a4c80b19db79a6874fc4a05f23d27478ea2
Max Desiatov <max@desiatov.com>
2021-06-21 16:00:28 +0100

    Attempt `padding` modifier fusion to avoid nested `div`s (#253)
    This allows fusing nested `.padding` modifiers into a single `div` that sums up padding values from all these modifiers.
    
    Before:
    
    ```swift
    Text("text").padding(10).padding(20)
    ```
    
    rendered to this (text styling omitted for brevity):
    
    ```html
    <div style="padding-top: 20.0px; padding-left: 20.0px; padding-bottom: 20.0px; padding-right: 20.0px;">
      <div style="padding-top: 10.0px; padding-left: 10.0px; padding-bottom: 10.0px; padding-right: 10.0px;">
        <span>text</span>
      </div>
    </div>
    ```
    
    Now it renders as
    
    ```html
    <div style="padding-top: 30.0px; padding-left: 30.0px; padding-bottom: 30.0px; padding-right: 30.0px;">
      <span>text</span>
    </div>
    ```
    
    I hope this approach could be applied to other modifier combinations where it makes sense (in separate PRs).
    
    * Attempt `padding` modifier fusion
    
    * Fix linter warning
    
    * Add a test to verify that fusion works
    
    * Enable fusion of modifiers nested three times
    
    * Filter out empty attributes
    
    * Run snapshot tests only on macOS for now
    
    * Fully exclude snapshot testing on WASI
    
    * Fix `testOptional` snapshot
    
    * Clean up code formatting

--- Package.swift
+++ Package.swift
@@ -56,6 +56,11 @@ let package = Package(
     .package(url: "https://github.com/OpenCombine/OpenCombine.git", from: "0.12.0"),
     .package(url: "https://github.com/swiftwasm/OpenCombineJS.git", .upToNextMinor(from: "0.1.1")),
     .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
+    .package(
+      name: "SnapshotTesting",
+      url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+      from: "1.9.0"
+    ),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define
@@ -180,7 +185,15 @@ let package = Package(
     ),
     .testTarget(
       name: "TokamakStaticHTMLTests",
-      dependencies: ["TokamakStaticHTML"]
+      dependencies: [
+        "TokamakStaticHTML",
+        .product(
+          name: "SnapshotTesting",
+          package: "SnapshotTesting",
+          condition: .when(platforms: [.macOS, .linux])
+        ),
+      ],
+      exclude: ["__Snapshots__"]
     ),
   ]
 )
