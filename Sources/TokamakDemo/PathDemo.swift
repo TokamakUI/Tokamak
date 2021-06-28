5a64d112b1d0d952f01cd82e7a95eb03581aa6de
Ezra Berch <ezraberch@mac.com>
2021-06-27 21:50:10 -0400

    change PathDemo, add LayoutTest

--- Sources/TokamakDemo/PathDemo.swift
+++ Sources/TokamakDemo/PathDemo.swift
@@ -31,13 +31,13 @@ struct Star: Shape {
 struct PathDemo: View {
   var body: some View {
     VStack {
-      ZStack {
+      HStack {
         Star()
           .fill(Color(red: 1, green: 0.75, blue: 0.1, opacity: 1))
         Circle()
-          .stroke(Color.red)
+          .stroke(Color.blue)
+          .frame(width: 80, height: 80, alignment: .center)
       }
-      .frame(width: 100, height: 100, alignment: .center)
       Path { path in
         path.addRect(.init(origin: .zero, size: .init(width: 20, height: 20)))
         path.addEllipse(in: .init(
