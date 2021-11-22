534784ca7bf73b5116567edb761b779126f45c62
Max Desiatov <max@desiatov.com>
2021-11-22 19:03:09 +0100

    Replace `compiler` with `swift` in some places

--- Sources/TokamakDemo/TokamakDemo.swift
+++ Sources/TokamakDemo/TokamakDemo.swift
@@ -133,7 +133,7 @@ struct TokamakDemoView: View {
           }
           Section(header: Text("Modifiers")) {
             NavItem("Shadow", destination: ShadowDemo())
-            #if os(WASI) && compiler(>=5.5)
+            #if os(WASI) && swift(>=5.5)
             NavItem("Task", destination: TaskDemo())
             #endif
           }
