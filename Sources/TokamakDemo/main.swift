d60aaa477586c93e49ef8b46ee8b5d6ed4f10947
Carson Katri <carson.katri@gmail.com>
2022-05-30 16:02:45 -0400

    Restore original demo

--- Sources/TokamakDemo/main.swift
+++ Sources/TokamakDemo/main.swift
@@ -14,76 +14,27 @@
 
 import TokamakShim
 
-struct TestModifier: ViewModifier {
-  func body(content: Content) -> some View {
-    content
-      .foregroundStyle(.secondary)
-      .padding()
-  }
-}
+@available(OSX 10.16, iOS 14.0, *)
+struct CustomScene: Scene {
+  @Environment(\.scenePhase) private var scenePhase
 
-struct TestView: View {
-  @State private var count = 0
-  var body: some View {
-    VStack {
-      Count(count: count)
-      Actions(count: $count)
-    }
-  }
-
-  struct Count: View {
-    let count: Int
-
-    var body: some View {
-      Text("Count: \(count)")
-    }
-  }
-
-  struct Actions: View {
-    @Binding var count: Int
-
-    var body: some View {
-      HStack {
-        if count > 0 {
-          Button("Decrement") {
-            self.count -= 1
-          }
-          .modifier(TestModifier())
-        }
-        Button("Increment") {
-          self.count += 1
-        }
-        .modifier(TestModifier())
-      }
+  var body: some Scene {
+    print("In CustomScene.body scenePhase is \(scenePhase)")
+    return WindowGroup("Tokamak Demo") {
+      TokamakDemoView()
     }
   }
 }
 
-#if os(WASI)
-let reconciler = DOMFiberRenderer("body", shouldLayout: true).render(TestView())
-#endif
+@available(OSX 10.16, iOS 14.0, *)
+struct TokamakDemoApp: App {
+  var body: some Scene {
+    CustomScene()
+  }
+}
 
-// @available(OSX 10.16, iOS 14.0, *)
-// struct CustomScene: Scene {
-//   @Environment(\.scenePhase) private var scenePhase
-
-//   var body: some Scene {
-//     print("In CustomScene.body scenePhase is \(scenePhase)")
-//     return WindowGroup("Tokamak Demo") {
-//       TokamakDemoView()
-//     }
-//   }
-// }
-
-// @available(OSX 10.16, iOS 14.0, *)
-// struct TokamakDemoApp: App {
-//   var body: some Scene {
-//     CustomScene()
-//   }
-// }
-
-// // If @main was supported for executable Swift Packages,
-// // this would match SwiftUI 100%
-// if #available(OSX 10.16, iOS 14.0, *) {
-//   TokamakDemoApp.main()
-// }
+// If @main was supported for executable Swift Packages,
+// this would match SwiftUI 100%
+if #available(OSX 10.16, iOS 14.0, *) {
+  TokamakDemoApp.main()
+}
