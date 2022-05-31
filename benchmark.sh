a08e0adf3636ba8dba54599140b775467fe2810e
Ezra Berch <ezraberch@mac.com>
2022-05-31 10:44:36 -0400

    * Add file size to benchmark script * Reenable macOS TokamakDemo build in CI

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,8 @@
 set -eux
 
 swift build -c release --product TokamakCoreBenchmark
+stat -f "TokamakCoreBenchmark: %z bytes" ./.build/release/TokamakCoreBenchmark
 ./.build/release/TokamakCoreBenchmark
 swift build -c release --product TokamakStaticHTMLBenchmark
+stat -f "TokamakStaticHTMLBenchmark: %z bytes" ./.build/release/TokamakStaticHTMLBenchmark
 ./.build/release/TokamakStaticHTMLBenchmark
