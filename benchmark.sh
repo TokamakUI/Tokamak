38a571bf8cf1688f3a1dc722800ba7f75f59b914
ezraberch <49635435+ezraberch@users.noreply.github.com>
2022-05-31 13:28:05 -0400

    Make sizes more readable

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,8 @@
 set -eux
 
 swift build -c release --product TokamakCoreBenchmark
-stat -f "::warning file=Sources/TokamakCoreBenchmark/main.swift,line=1,col=1::TokamakCoreBenchmark is %z bytes." ./.build/release/TokamakCoreBenchmark
+ls -lh ./.build/release/TokamakCoreBenchmark | awk '{printf  "::warning file=Sources/TokamakCoreBenchmark/main.swift,line=1,col=1::TokamakCoreBenchmark is %s.",$5}'
 ./.build/release/TokamakCoreBenchmark
 swift build -c release --product TokamakStaticHTMLBenchmark
-stat -f "::warning file=Sources/TokamakStaticHTMLBenchmark/main.swift,line=1,col=1::TokamakStaticHTMLBenchmark is %z bytes." ./.build/release/TokamakStaticHTMLBenchmark
+ls -lh ./.build/release/TokamakStaticHTMLBenchmark | awk '{printf  "::warning file=Sources/TokamakStaticHTMLBenchmark/main.swift,line=1,col=1::TokamakStaticHTMLBenchmark is %s.",$5}'
 ./.build/release/TokamakStaticHTMLBenchmark
