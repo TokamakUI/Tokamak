bd699120a5b21191db33681dace42a9ae1c85c6f
ezraberch <49635435+ezraberch@users.noreply.github.com>
2022-05-31 11:57:55 -0400

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,8 @@
 set -eux
 
 swift build -c release --product TokamakCoreBenchmark
-stat -f "TokamakCoreBenchmark: %z bytes" ./.build/release/TokamakCoreBenchmark
+stat -f "::warning file=Sources/TokamakCoreBenchmark/main.swift,line=1,col=1::TokamakCoreBenchmark is %z bytes." ./.build/release/TokamakCoreBenchmark
 ./.build/release/TokamakCoreBenchmark
 swift build -c release --product TokamakStaticHTMLBenchmark
-stat -f "TokamakStaticHTMLBenchmark: %z bytes" ./.build/release/TokamakStaticHTMLBenchmark
+stat -f "::warning file=Sources/TokamakStaticHTMLBenchmark/main.swift,line=1,col=1::TokamakStaticHTMLBenchmark is %z bytes." ./.build/release/TokamakStaticHTMLBenchmark
 ./.build/release/TokamakStaticHTMLBenchmark
