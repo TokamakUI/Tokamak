0b182d99a1dd9f541fe58eaf611a3f12648f619d
ezraberch <49635435+ezraberch@users.noreply.github.com>
2022-06-24 08:36:35 -0400

    Add file size to benchmark script (#496)
    * * Add file size to benchmark script
    * Reenable macOS TokamakDemo build in CI
    
    * Update benchmark.sh
    
    * Add Wasm size annotation
    
    * Make sizes more readable

--- benchmark.sh
+++ benchmark.sh
@@ -3,6 +3,8 @@
 set -eux
 
 swift build -c release --product TokamakCoreBenchmark
+ls -lh ./.build/release/TokamakCoreBenchmark | awk '{printf  "::warning file=Sources/TokamakCoreBenchmark/main.swift,line=1,col=1::TokamakCoreBenchmark is %s.",$5}'
 ./.build/release/TokamakCoreBenchmark
 swift build -c release --product TokamakStaticHTMLBenchmark
+ls -lh ./.build/release/TokamakStaticHTMLBenchmark | awk '{printf  "::warning file=Sources/TokamakStaticHTMLBenchmark/main.swift,line=1,col=1::TokamakStaticHTMLBenchmark is %s.",$5}'
 ./.build/release/TokamakStaticHTMLBenchmark
