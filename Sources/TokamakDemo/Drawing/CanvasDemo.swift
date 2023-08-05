75d9c708fa3006eede5beffe2ef946511d7823bf
furby™ <devs@wabi.foundation>
2023-08-05 17:30:15 -0500

    Keep TimelineView in demo swift(<5.9)?

--- Sources/TokamakDemo/Drawing/CanvasDemo.swift
+++ Sources/TokamakDemo/Drawing/CanvasDemo.swift
@@ -62,37 +62,38 @@ struct Confetti: View {
   static let shape = Rectangle()
 
   var body: some View {
-    EmptyView()
-    //   TimelineView(AnimationTimelineSchedule.animation) { timeline in
-    //     Canvas { context, size in
-    //       let elapsed = CGFloat(
-    //         timeline.date.timeIntervalSince1970 - startDate
-    //           .timeIntervalSince1970
-    //       ).truncatingRemainder(dividingBy: 4) * 10
-    //       let sqElapsed = elapsed * elapsed
+    #if swift(<5.9)
+    TimelineView(AnimationTimelineSchedule.animation) { timeline in
+      Canvas { context, size in
+        let elapsed = CGFloat(
+          timeline.date.timeIntervalSince1970 - startDate
+            .timeIntervalSince1970
+        ).truncatingRemainder(dividingBy: 4) * 10
+        let sqElapsed = elapsed * elapsed
 
-    //       for piece in pieces {
-    //         context.drawLayer { context in
-    //           context.translateBy(
-    //             x: (piece.point.x * size.width) + (piece.initialVelocity.width * sqElapsed),
-    //             y: -((1 + piece.point.y) * (size.height / 2)) + (1 + piece.initialVelocity.height) *
-    //               sqElapsed
-    //           )
-    //           context.rotate(by: Angle.degrees(25 * piece.angularVelocity * Double(elapsed)))
-    //           context.fill(
-    //             Self.shape
-    //               .path(in: CGRect(
-    //                 origin: .zero,
-    //                 size: CGSize(
-    //                   width: 5 + (20 * piece.size.width),
-    //                   height: 5 + (20 * piece.size.height)
-    //                 )
-    //               )),
-    //             with: .color(piece.color)
-    //           )
-    //         }
-    //       }
-    //     }
-    //   }
+        for piece in pieces {
+          context.drawLayer { context in
+            context.translateBy(
+              x: (piece.point.x * size.width) + (piece.initialVelocity.width * sqElapsed),
+              y: -((1 + piece.point.y) * (size.height / 2)) + (1 + piece.initialVelocity.height) *
+                sqElapsed
+            )
+            context.rotate(by: Angle.degrees(25 * piece.angularVelocity * Double(elapsed)))
+            context.fill(
+              Self.shape
+                .path(in: CGRect(
+                  origin: .zero,
+                  size: CGSize(
+                    width: 5 + (20 * piece.size.width),
+                    height: 5 + (20 * piece.size.height)
+                  )
+                )),
+              with: .color(piece.color)
+            )
+          }
+        }
+      }
+    }
+    #endif /* swift(<5.9) */
   }
 }
